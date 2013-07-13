acecd7ee346b4e852bead27502f07663b2c265e6
Ron Kurr <kurr@kurron.org>
2013-07-13 18:19:46 -0400

    Added a user to each machine

---
+++ multiple-machines/add-user.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+USERNAME=logan
+
+# make sure we are idempotent
+if [ -f "/var/vagrant_add_user" ]; then
+    exit 0
+fi
+
+# Actual shell commands here.
+echo "Adding user " $USERNAME
+useradd $USERNAME -m -s /bin/bash
+addgroup $USERNAME
+adduser $USERNAME $USERNAME
+adduser $USERNAME adm
+adduser $USERNAME cdrom
+adduser $USERNAME sudo
+adduser $USERNAME dip
+adduser $USERNAME plugdev
+adduser $USERNAME lpadm
+adduser $USERNAME sambashare
+
+
+# signal a successful provision
+touch /var/vagrant_add_user
