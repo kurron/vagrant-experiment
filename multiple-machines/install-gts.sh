863bd89611f10b00be76f29c42a079c28536ef80
kurron <kurr@kurron.org>
2013-07-14 01:16:30 -0400

    Install GTS.

---
+++ multiple-machines/install-gts.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+ACCOUNT=$1
+DONEFILE=/var/vagrant-gts
+CACHE=/vagrant/file-cache
+ARCHIVE=gts.tar.gz
+ACCOUNTDIR=/home/${ACCOUNT}
+GTSDIR=${ACCOUNTDIR}/Software
+
+# make sure we are idempotent
+if [ -f "${DONEFILE}" ]; then
+    exit 0
+fi
+
+# Actual shell commands here.
+echo "Installing Groovy Tool Suite..."
+mkdir -p ${CACHE}
+if [ -f "${CACHE}/${ARCHIVE}" ]; then
+  echo "Groovy Tool Suite already cached.  Not downloading." 
+else
+  echo "Downloading Groovy Tool Suite..."
+  wget --quiet --output-document=${CACHE}/${ARCHIVE} http://dist.springsource.com/release/STS/3.3.0/dist/e4.3/groovy-grails-tool-suite-3.3.0.RELEASE-e4.3-linux-gtk-x86_64.tar.gz 
+fi
+mkdir -p ${GTSDIR}
+tar --gzip --extract --file=${CACHE}/${ARCHIVE} --directory=${GTSDIR}
+chown -R ${ACCOUNT}:${ACCOUNT} ${ACCOUNTDIR}
+
+# signal a successful provision
+touch ${DONEFILE} 
