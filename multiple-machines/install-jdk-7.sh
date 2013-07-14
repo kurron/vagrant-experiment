158084841f1506a67f2bcabb58c35b435b2c9f60
kurron <kurr@kurron.org>
2013-07-13 21:39:38 -0400

    We now cache the downloads

--- multiple-machines/install-jdk-7.sh
+++ multiple-machines/install-jdk-7.sh
@@ -7,10 +7,16 @@ fi
 
 # Actual shell commands here.
 echo "Installing Oracle JDK 7..."
-echo "Downloading Oracle JDK 7..."
-wget --quiet --output-document=/tmp/jdk7.gz http://home.comcast.net/~kurr/vagrant/jdk-7u25-linux-x64.gz
+mkdir -p /vagrant/file-cache
+if [ -f "/vagrant/file-cache/jdk7.gz" ]; then
+  echo "JDK already cached.  Not downloading." 
+else
+  echo "Downloading Oracle JDK 7..."
+  wget --quiet --output-document=/vagrant/file-cache/jdk7.gz http://home.comcast.net/~kurr/vagrant/jdk-7u25-linux-x64.gz
+fi
 mkdir -p /usr/lib/jvm
-tar --gzip --extract --verbose --file=/tmp/jdk7.gz --directory=/usr/lib/jvm
+tar --gzip --extract --verbose --file=/vagrant/file-cache/jdk7.gz --directory=/usr/lib/jvm
+chown -R root:root /usr/lib/jvm/jdk1.7.0_25
 cd /usr/lib/jvm ; ln -s jdk1.7.0_25 oracle-jdk-7 ; ls -lh
 
 # signal a successful provision
--- multiple-machines/install-tomcat-7.sh
+++ multiple-machines/install-tomcat-7.sh
@@ -9,10 +9,17 @@ fi
 echo "Installing Tomcat 7..."
 #aptitude install -y tomcat7 
 #aptitude install -y tomcat7-user 
-echo "Downloading Tomcat 7..."
-wget --quiet --output-document=/tmp/tomcat.tar.gz http://home.comcast.net/~kurr/vagrant/apache-tomcat-7.0.42.tar.gz
+mkdir -p /vagrant/file-cache
+if [ -f "/vagrant/file-cache/tomcat.tar.gz" ]; then
+    echo "Tomcat archive already cached. Not downloading." 
+else
+    echo "Downloading Tomcat 7..."
+    wget --quiet --output-document=/vagrant/file-cache/tomcat.tar.gz http://home.comcast.net/~kurr/vagrant/apache-tomcat-7.0.42.tar.gz
+fi
+
 mkdir -p /usr/lib/tomcat
-tar --gzip --extract --verbose --file=/tmp/tomcat.tar.gz --directory=/usr/lib/tomcat
+tar --gzip --extract --verbose --file=/vagrant/file-cache/tomcat.tar.gz --directory=/usr/lib/tomcat
+chown -R root:root /usr/lib/tomcat/apache-tomcat-7.0.42
 cd /usr/lib/tomcat ; ln -s apache-tomcat-7.0.42 tomcat-7 ; ls -lh
 
 # signal a successful provision
