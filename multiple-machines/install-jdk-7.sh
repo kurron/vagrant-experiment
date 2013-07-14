519f4cf1c8e07546909425566ef854ff84342eb2
kurron <kurr@kurron.org>
2013-07-13 21:54:51 -0400

    Made the scripts DRY.

--- multiple-machines/install-jdk-7.sh
+++ multiple-machines/install-jdk-7.sh
@@ -6,18 +6,21 @@ if [ -f "/var/vagrant_jdk_7" ]; then
 fi
 
 # Actual shell commands here.
+CACHE=/vagrant/file-cache
+ARCHIVE=jdk7.tar.gz
+JVMDIR=/usr/lib/jvm
 echo "Installing Oracle JDK 7..."
-mkdir -p /vagrant/file-cache
-if [ -f "/vagrant/file-cache/jdk7.gz" ]; then
+mkdir -p ${CACHE}
+if [ -f "${CACHE}/${ARCHIVE}" ]; then
   echo "JDK already cached.  Not downloading." 
 else
   echo "Downloading Oracle JDK 7..."
-  wget --quiet --output-document=/vagrant/file-cache/jdk7.gz http://home.comcast.net/~kurr/vagrant/jdk-7u25-linux-x64.gz
+  wget --quiet --output-document=${CACHE}/${ARCHIVE} http://home.comcast.net/~kurr/vagrant/jdk-7u25-linux-x64.gz
 fi
-mkdir -p /usr/lib/jvm
-tar --gzip --extract --verbose --file=/vagrant/file-cache/jdk7.gz --directory=/usr/lib/jvm
-chown -R root:root /usr/lib/jvm/jdk1.7.0_25
-cd /usr/lib/jvm ; ln -s jdk1.7.0_25 oracle-jdk-7 ; ls -lh
+mkdir -p ${JVMDIR}
+tar --gzip --extract --verbose --file=${CACHE}/${ARCHIVE} --directory=${JVMDIR}
+chown -R root:root ${JVMDIR}/jdk1.7.0_25
+cd ${JVMDIR} ; ln -s jdk1.7.0_25 oracle-jdk-7 ; ls -lh
 
 # signal a successful provision
 touch /var/vagrant_jdk_7
--- multiple-machines/install-tomcat-7.sh
+++ multiple-machines/install-tomcat-7.sh
@@ -6,21 +6,25 @@ if [ -f "/var/vagrant_tomcat_7" ]; then
 fi
 
 # Actual shell commands here.
+CACHE=/vagrant/file-cache
+ARCHIVE=tomcat.tar.gz
+TOMCATDIR=/usr/lib/apache-tomcat
+
 echo "Installing Tomcat 7..."
 #aptitude install -y tomcat7 
 #aptitude install -y tomcat7-user 
-mkdir -p /vagrant/file-cache
-if [ -f "/vagrant/file-cache/tomcat.tar.gz" ]; then
+mkdir -p ${CACHE}
+if [ -f "${CACHE}/${ARCHIVE}" ]; then
     echo "Tomcat archive already cached. Not downloading." 
 else
     echo "Downloading Tomcat 7..."
-    wget --quiet --output-document=/vagrant/file-cache/tomcat.tar.gz http://home.comcast.net/~kurr/vagrant/apache-tomcat-7.0.42.tar.gz
+    wget --quiet --output-document=${CACHE}/${ARCHIVE} http://home.comcast.net/~kurr/vagrant/apache-tomcat-7.0.42.tar.gz
 fi
 
-mkdir -p /usr/lib/tomcat
-tar --gzip --extract --verbose --file=/vagrant/file-cache/tomcat.tar.gz --directory=/usr/lib/tomcat
-chown -R root:root /usr/lib/tomcat/apache-tomcat-7.0.42
-cd /usr/lib/tomcat ; ln -s apache-tomcat-7.0.42 tomcat-7 ; ls -lh
+mkdir -p ${TOMCATDIR}
+tar --gzip --extract --verbose --file=${CACHE}/${ARCHIVE} --directory=${TOMCATDIR}
+chown -R root:root ${TOMCATDIR}/apache-tomcat-7.0.42
+cd ${TOMCATDIR} ; ln -s apache-tomcat-7.0.42 tomcat-7 ; ls -lh
 
 # signal a successful provision
 touch /var/vagrant_tomcat_7
