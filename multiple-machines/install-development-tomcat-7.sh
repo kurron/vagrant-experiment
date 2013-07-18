81c75f1305b19ba2db789ba8233c1ee1b298f226
Ron Kurr <kurr@kurron.org>
2013-07-18 09:07:33 -0400

    Install Tomcat 7 development style

---
+++ multiple-machines/install-development-tomcat-7.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+ACCOUNT=$1
+DONEFILE=/var/vagrant-development-tomcat-7
+CACHE=/vagrant/file-cache
+ARCHIVE=tomcat.tar.gz
+ACCOUNTDIR=/home/${ACCOUNT}
+TARGET=${ACCOUNTDIR}/Software/Tomcat
+
+# make sure we are idempotent
+if [ -f "${DONEFILE}" ]; then
+    exit 0
+fi
+
+# Actual shell commands here.
+echo "Installing Tomcat 7 (development style)..."
+mkdir -p ${CACHE}
+if [ -f "${CACHE}/${ARCHIVE}" ]; then
+    echo "Tomcat archive already cached. Not downloading." 
+else
+    echo "Downloading Tomcat 7..."
+    wget --quiet --output-document=${CACHE}/${ARCHIVE} http://apache.mesi.com.ar/tomcat/tomcat-7/v7.0.42/bin/apache-tomcat-7.0.42.tar.gz 
+fi
+mkdir -p ${TARGET}
+tar --gzip --extract --file=${CACHE}/${ARCHIVE} --directory=${TARGET}
+cd ${TARGET} ; ln -s apache-tomcat-7.0.42 tomcat-7 ; ls -lh
+chown -R ${ACCOUNT}:${ACCOUNT} ${ACCOUNTDIR}
+
+# signal a successful provision
+touch ${DONEFILE} 
