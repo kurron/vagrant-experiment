e7e7645db53b267724be8bb9f2435b6dd2fc38c5
Ron Kurr <kurr@kurron.org>
2013-07-23 08:57:06 -0400

    We now install the current version of MongoDB from 10gen

--- multiple-machines/install-mongo.sh
+++ multiple-machines/install-mongo.sh
@@ -8,8 +8,13 @@ if [ -f "${DONEFILE}" ]; then
 fi
 
 # Actual shell commands here.
-echo "Installing MongoDB..."
-aptitude install -y mongodb 
+echo "Installing MongoDB from 10gen..."
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
+aptitude update
+aptitude install mongodb-10gen
+
+#aptitude install -y mongodb 
 
 # signal a successful provision
 touch ${DONEFILE}
