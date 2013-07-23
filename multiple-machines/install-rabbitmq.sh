cc2e3b3553a0dd6bb57b51a039440590ee4a09de
Ron Kurr <kurr@kurron.org>
2013-07-23 09:16:41 -0400

    We now install RabbitMQ from official repositories

--- multiple-machines/install-rabbitmq.sh
+++ multiple-machines/install-rabbitmq.sh
@@ -9,6 +9,9 @@ fi
 
 # Actual shell commands here.
 echo "Installing RabbitMQ Server..."
+cd /tmp ; wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc ; apt-key add rabbitmq-signing-key-public.asc
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | tee /etc/apt/sources.list.d/rabbitmq.list
+aptitude update
 aptitude install -y rabbitmq-server 
 
 # signal a successful provision
